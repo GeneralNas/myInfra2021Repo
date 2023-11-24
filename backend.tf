6a194b1c1d4d1f2cd15138a5eb707f4d47faa702
GeneralNas <108949367+GeneralNas@users.noreply.github.com>
2023-11-23 20:41:13 -0600

    Updated backend.tf by modifying dynamodb

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "nas-dev-tf-state-bucket"
     key = "main"
     region = "us-east-2"
-    dynamodb_table = "my-dynamo-db-table-tf"
+    dynamodb_table = "my-dynamodb-table"
   }
 }
